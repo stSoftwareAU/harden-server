2709004993be39ceef9a2de34e71fab58728362d
stSoftware <nigel@stsoftware.com.au>
2016-07-14 14:33:26 +1000

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -4,14 +4,14 @@ DAILY=dumps/`date +%a`
 mkdir -p $DAILY
 MONTHLY=dumps/`date +%b`
 mkdir -p $MONTHLY
-LIST=$(psql -tqc 'SELECT datname FROM pg_database where datistemplate = false;')
+LIST=$(psql -h localhost -U postgres -tqc 'SELECT datname FROM pg_database where datistemplate = false;')
 start_date=$(date +%Y%m%d-%T)
 echo $start_date "START pg_dump database(s) !" 
 for d in $LIST
 do
   temp_date_s=$(date +%Y%m%d-%T)
   echo $temp_date_s + "Database: " $d 
-  pg_dump $d |gzip -c > $DAILY/$d.gz
+  pg_dump -h localhost -U postgres $d |gzip -c > $DAILY/$d.gz
 done
 end_date=$(date +%Y%m%d-%T)
 echo $end_date "END pg_dump database(s) !"
