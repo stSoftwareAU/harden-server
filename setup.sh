1a25af0161d25e09be842be02e2f9703e51f6436
stSoftware <nigel@stsoftware.com.au>
2016-07-14 12:13:16 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -291,9 +291,12 @@ defaults() {
         fi
 }
 allowHosts(){
-cat /etc/hosts.allow | egrep -v "(192\.|203\.206\.176\.223|)" >/tmp/hosts.allow
-echo "sshd: 192.168." >> /tmp/hosts.allow
-echo "sshd: 203.206.176.223" >> /tmp/hosts.allow
+cat /etc/hosts.allow | egrep -v "(192\.168\.7\.|\#ST)" >/tmp/hosts.allow
+echo "sshd: 192.168.7.      #Local " >> /tmp/hosts.allow
+echo "sshd: 60.241.239.222  #ST Office iinet" >> /tmp/hosts.allow
+echo "sshd: 58.108.224.217  #ST Office optus" >> /tmp/hosts.allow
+echo "sshd: 101.0.96.194    #ST www1"
+echo "sshd: 101.0.106.2     #ST www2"
 cp /tmp/hosts.allow /etc/hosts.allow
 
 cat /etc/hosts.deny | egrep -v "sshd" >/tmp/hosts.deny
