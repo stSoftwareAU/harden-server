8dee3c701dcf5529f2170eef9fdff7a608575e1d
stSoftware <nigel@stsoftware.com.au>
2016-07-18 16:10:02 +1000

    config apache

--- setupApache.sh
+++ setupApache.sh
@@ -6,3 +6,25 @@ sed --in-place='.bak' -r 's/^[\t #]+SSLStrictSNIVHostCheck .*$/     SSLStrictSNI
 
 rm /etc/apache2/mods-enabled/ssl.conf
 cp /tmp/ssl.conf /etc/apache2/mods-enabled/ssl.conf
+mkdir -p /home/webapps/apache
+
+if [ ! -f /home/webapps/apache/000-default.conf ]; then
+  cp /etc/apache2/sites-enabled/000-default.conf /home/webapps/apache/
+  rm /etc/apache2/sites-enabled/000-default.conf
+  
+  ln -s /home/webapps/apache/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+fi
+
+if [ ! -f /home/webapps/apache/httpd-jk.conf ]; then
+  cp /etc/libapache2-mod-jk/httpd-jk.conf /home/webapps/apache/
+  rm /etc/libapache2-mod-jk/httpd-jk.conf
+  ln -s /home/webapps/apache/httpd-jk.conf /etc/libapache2-mod-jk/
+fi
+
+if [ ! -f /home/webapps/apache/workers.properties ]; then
+  cp /etc/libapache2-mod-jk/workers.properties /home/webapps/apache/
+  rm /etc/libapache2-mod-jk/workers.properties
+  ln -s /home/webapps/apache/workers.properties /etc/libapache2-mod-jk/
+fi
+chown -R webapps /home/webapps/apache
+chmod -R go-wrx /home/webapps/apache
