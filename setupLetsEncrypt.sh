62f0364291dc1f08ec7704eb8b49b64c376b53d0
stSoftware <nigel@stsoftware.com.au>
2016-07-27 21:06:13 +1000

    Update setupLetsEncrypt.sh

--- setupLetsEncrypt.sh
+++ setupLetsEncrypt.sh
@@ -21,5 +21,30 @@ fetchFiles() {
         chown letsencrypt:www-data /home/letsencrypt/acme_tiny.py
 }
 
+generateKeys(){
+   cd /home/letsencrypt
+
+   if [ ! -f account.key ]; then
+       openssl genrsa 4096 > account.key
+   fi
+   if [ ! -f domain.key ]; then
+        #generate a domain private key (if you haven't already)
+        openssl genrsa 4096 > domain.key
+   fi
+
+   if [ ! -d challenges ]; then
+        #make some challenge folder
+        mkdir -p challenges
+   fi
+
+   if [ ! -f lets-encrypt-x3-cross-signed.pem ]; then
+        wget -N https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem
+   fi
+   chown -R letsencrypt:www-data /home/letsencrypt/
+   chmod 600 /home/letsencrypt/*key
+   chmod -R go-xrw /home/letsencrypt/
+}
+
 addUser;
 fetchFiles;
+generateKeys;
