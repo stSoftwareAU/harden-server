a8bf2331ea290d574133759d87b2c44086aca44c
stSoftware <nigel@stsoftware.com.au>
2016-07-27 19:27:48 +1000

    fetch files.

---
+++ setupLetsEncrypt.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+addUser( ) {
+        ret=false
+        user="letsencrypt"
+        getent passwd $user >/dev/null 2>&1 && ret=true
+
+        if $ret; then
+            echo "User '$user' exists"
+        else
+            useradd -g www-data -m -s /bin/bash $user
+        fi
+}
+
+fetchFiles() {
+        cd /tmp
+        rm acme_tiny.py
+        wget https://raw.githubusercontent.com/stSoftwareAU/acme-cluster/master/acme_tiny.py
+        
+        cp acme_tiny.py /home/lersencrypt/
+        chown letsencrypt:letsencrypt /home/lersencrypt/acme_tiny.py
+}
+
+addUser();
+fetchFiles();
