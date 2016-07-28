24525e97a48b6278560dd053b8c4efde1f3bbceb
stSoftware <nigel@stsoftware.com.au>
2016-07-28 16:15:40 +1000

    Update setupApache.sh

--- setupApache.sh
+++ setupApache.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 cp /etc/apache2/mods-enabled/ssl.conf /tmp/ssl.conf
 sed --in-place='.bak' -r 's/^[\t ]+SSLCipherSuite.*$/\tSSLCipherSuite "EECDH+ECDSA+AESGCM EECDH+aRSA+AESGCM EECDH+ECDSA+SHA384 EECDH+ECDSA+SHA256 EECDH+aRSA+SHA384 EECDH+aRSA+SHA256 EECDH+aRSA+RC4 EECDH EDH+aRSA !RC4 !aNULL !eNULL !LOW !3DES !MD5 !EXP !PSK !SRP !DSS"/m' ssl.conf
 sed --in-place='.bak' -r 's/^[\t #]+SSLHonorCipherOrder .*$/     SSLHonorCipherOrder on/g' /tmp/ssl.conf
