3b87db1e201ace6d16b2932d9c708fb5375a989d
stSoftware <nigel@stsoftware.com.au>
2016-07-30 18:46:24 +1000

    Update setupApache.sh

--- setupApache.sh
+++ setupApache.sh
@@ -6,7 +6,7 @@ openssl version > /tmp/openssl.version
  
 cp /etc/apache2/mods-enabled/ssl.conf /tmp/ssl.conf
 #sed --in-place='.bak' -r 's/^[\t ]+SSLCipherSuite.*$/\tSSLCipherSuite "EECDH+ECDSA+AESGCM EECDH+aRSA+AESGCM EECDH+ECDSA+SHA384 EECDH+ECDSA+SHA256 EECDH+aRSA+SHA384 EECDH+aRSA+SHA256 EECDH+aRSA+RC4 EECDH EDH+aRSA !RC4 !aNULL !eNULL !LOW !3DES !MD5 !EXP !PSK !SRP !DSS"/m' ssl.conf
-sed --in-place='.bak' -r 's/^[\t ]+SSLCipherSuite.*$/\tSSLCipherSuite "HIGH:!aNULL:!MD5:!3DES:!CAMELLIA:!AES128"/m' ssl.conf
+sed --in-place='.bak' -r 's/^[\t ]+SSLCipherSuite.*$/\tSSLCipherSuite "HIGH:!aNULL:!MD5:!3DES:!CAMELLIA"/m' ssl.conf
 sed --in-place='.bak' -r 's/^[\t #]+SSLHonorCipherOrder .*$/     SSLHonorCipherOrder on/g' /tmp/ssl.conf
 #sed --in-place='.bak' -r 's/^[\t #]+SSLProtocol .*$/     SSLProtocol all -SSLv3 -SSLv2/g' /tmp/ssl.conf
 sed --in-place='.bak' -r 's/^[\t #]+SSLProtocol .*$/     SSLProtocol TLSv1.2/g' /tmp/ssl.conf
