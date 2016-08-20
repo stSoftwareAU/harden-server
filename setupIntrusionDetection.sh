6d7301f034366e71005b9e1480e356f9c48d3722
stSoftware <nigel@stsoftware.com.au>
2016-08-20 13:31:02 +1000

    Update setupIntrusionDetection.sh

--- setupIntrusionDetection.sh
+++ setupIntrusionDetection.sh
@@ -22,7 +22,7 @@ set +e
 crontab -l > /tmp/crontab.txt
 set -e
 if ! grep -q "/var/log/id-scan.txt" /tmp/crontab.txt; then
-  echo "22 * * * * nice ls -laRL /bin /root /boot /etc /opt /usr > /var/log/id-scan.working;mv /var/log/id-scan.working /var/log/id-scan.txt" >> /tmp/crontab.txt
+  echo "22 * * * * nice ls -laR /bin /root /boot /etc /opt /usr > /var/log/id-scan.working 2>1;mv /var/log/id-scan.working /var/log/id-scan.txt" >> /tmp/crontab.txt
   crontab < /tmp/crontab.txt
 fi
 rm /tmp/crontab.txt
