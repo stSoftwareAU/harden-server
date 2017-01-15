0bfc85a8179c8dbbe68c6597a814b6272d58f4c1
nleck <nigel@stsoftware.com.au>
2017-01-15 11:10:10 +1100

    assume yes

--- bin/updateOS.sh
+++ bin/updateOS.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade -y
 sudo apt-get autoclean
 sudo apt-get dist-upgrade
 sudo apt-get check
-sudo apt-get autoremove
+sudo apt-get autoremove -y
 sudo update-grub
 
 if [ -f /etc/init.d/php7.0-fpm ]; then
