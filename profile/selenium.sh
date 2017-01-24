d5ba8422beab5dd9549b7603c4efb7bffa453404
parminder <preetzdomain@gmail.com>
2017-01-24 12:55:50 +1100

    selenium config

---
+++ profile/selenium.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+if (( $EUID == 0 )); then
+    echo "Please do not run as root."
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+
+./devserver.sh
+
+if ! sudo grep -q -e "jenkins" "/etc/sudoers"; then
+    sudo cp /etc/sudoers /tmp/sudoers
+    sudo chmod go+rw /tmp/sudoers
+
+    echo "jenkins ALL = (selenium) NOPASSWD: /home/selenium/config-selenium/tp_web.sh" >> /tmp/sudoers
+    sudo chmod go-rw /tmp/sudoers
+    sudo cp /tmp/sudoers /etc/sudoers
+fi
+
+
